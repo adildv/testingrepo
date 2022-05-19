22cee7012e44c76f05838a248cadeed14afd0ddb
devp-adil <awnhashmi@gmail.com>
2022-05-19 11:30:25 +0530

    some changes

---
+++ .history/index_20220519113003.php
@@ -0,0 +1,27 @@
+<?php
+class Class1
+{
+
+  function __construct()
+  {
+      echo 'im construct from 1 ';
+  }
+  function fun1()
+  {
+      echo 'hey im fun1';
+  }
+}
+
+class Class2 extends Class1
+{
+    function __construct()
+    {
+        echo 'im constr from 6 ';
+        parent::__construct();
+
+    }   
+}
+
+
+$obj2 = new Class2();
+?>
